79572d26cccbea0d672d1ffca5517f2bed36ca9f
ngovantoanttb <ngovantoan1305y@gmai.com>
2024-11-09 02:24:42 +0700

    Add Button component with various styles and props handling

---
+++ src/components/Button/index.js
@@ -0,0 +1,67 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    small = false,
+    large = false,
+    children,
+    text = false,
+    disabled = false,
+    rounded = false,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove event listener when button is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+    const classes = cx('wrapper', {
+        primary,
+        outline,
+        text,
+        small,
+        large,
+        rounded,
+        disabled,
+        [className]: className,
+        
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <san className={cx('icon')}>{leftIcon}</san>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
